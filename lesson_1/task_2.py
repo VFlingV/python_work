eac4803c0a04aa5d25d51d6ad9f0772a19558559
Eduard <Nollkol>
2022-09-06 23:18:43 +0500

    new_test_1

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -4,11 +4,19 @@
 - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 -
 '''
-
-a = [5]
+n = 0
+a = int(input('введите целое число = '))
+my_list = []
 result = []
-for i in range(1, 5 + 1 ):
-
-    result.append(i)
-
+for i in range(a):
+    my_list.append(i+1)
+b = my_list[0]
+while n < a:
+    b = b * my_list[n]
+    result.append(b)
+    n += 1
 print(result)
+
+
+
+
